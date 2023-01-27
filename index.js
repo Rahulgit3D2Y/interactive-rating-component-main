4089380f33b9eed782da8c7df872c491863cc988
Rahulgit3D2Y <109753648+Rahulgit3D2Y@users.noreply.github.com>
2023-01-27 20:39:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function sub()
+{
+    document.getElementById('main').style.display="none"
+     
+    if(document.getElementById('btn-1').focus==true)
+    {
+      document.getElementById('span').innerHTML=document.getElementById('btn-1').innerHTML
+    }
+    else if(document.getElementById('btn-3').focus())
+    {
+        document.getElementById('span').innerHTML=document.getElementById('btn-3').focus()
+    }
+}
