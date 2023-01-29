835a72df7ca25de6912ca73aa3c3b91ed592cf9c
rahul3d2y <rc2504000@gmail.com>
2023-01-29 14:27:04 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 function sub()
 {
     document.getElementById('main').style.display="none"
-     
-    if(document.getElementById('btn-1').focus==true)
-    {
-      document.getElementById('span').innerHTML=document.getElementById('btn-1').innerHTML
-    }
-    else if(document.getElementById('btn-3').focus())
-    {
-        document.getElementById('span').innerHTML=document.getElementById('btn-3').focus()
-    }
+    document.getElementById('sub').style.display="flex"
+}
+
+function val()
+{
+  document.getElementById('span').innerHTML=document.getElementById('btn-1').innerHTML
+}
+function val2()
+{
+  document.getElementById('span').innerHTML=document.getElementById('btn-2').innerHTML
+}
+function val3()
+{
+  document.getElementById('span').innerHTML=document.getElementById('btn-3').innerHTML
+}
+function val4()
+{
+  document.getElementById('span').innerHTML=document.getElementById('btn-4').innerHTML
+}
+function val5()
+{
+  document.getElementById('span').innerHTML=document.getElementById('btn-5').innerHTML
 }
